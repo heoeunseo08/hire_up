82c60c68bddaf1f3e1e7a1dd447d1e8c2df062b7
heoeunseo <h.eunseo6930@gmail.com>
2026-06-26 00:11:09 +0900

    먀 vpdlwl wnd

---
+++ lib/screens/B/ai_log_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AiLogScreen extends StatefulWidget {
+  const AiLogScreen({super.key});
+
+  @override
+  State<AiLogScreen> createState() => _AiLogScreenState();
+}
+
+class _AiLogScreenState extends State<AiLogScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/screens/B/ai_play_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:hire_up/utils/info.dart';
+
+class AiPlayScreen extends StatefulWidget {
+  const AiPlayScreen({super.key});
+
+  @override
+  State<AiPlayScreen> createState() => _AiPlayScreenState();
+}
+
+class _AiPlayScreenState extends State<AiPlayScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: bgColor,
+    );
+  }
+}
